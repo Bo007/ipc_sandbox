7e153a94f6ec7797e879fe42061014704f6b5b73
Oleksii Bozhenko <oleksii.bozhenko@spacelabs.com>
2021-10-05 00:22:42 +0300

    added work plane

--- ServerSettings.h
+++ ServerSettings.h
@@ -21,3 +21,18 @@ int openFifo(const char* filename, int flags)
     }
     return error;
 }
+
+
+// TODO:
+// we have to implement communication protocol between client and server
+// client has to not just open file but sent message "connect_request" to server
+// unless server sends it back message "connected"
+// after this client can send data to the server with message "proceed data"
+// server revieves the data and sends message "processing" and manipulate with data
+// after this, server sents "result"
+// and client sends "disconneted"
+// each message must have client id(process id will be ok for now)
+// server should process data in separate thread
+// server should disconect client after some delay if the client is idle
+// server has to send message "unknown_client" if client isn't connected
+// client has to be able reconnect after "unknown_client" from the server
